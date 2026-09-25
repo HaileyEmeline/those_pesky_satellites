86554d4af5363c9c90bd67687ee3294646568c3c
HaileyEmeline <hschoppe@uvm.edu>
2026-09-24 22:49:05 -0400

    satellite positions download

--- collect_satellites.py
+++ collect_satellites.py
@@ -1,3 +1,12 @@
 from skyfield.api import load 
 
 #TODO: Pull all satellites, track only those above horizon and within x range of FOV, print them
+
+max_days = 0.5
+name = 'stations.json'
+
+url = "https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=json"
+
+if not load.exists(name) or load.days_old(name) >= max_days:
+    load.download(url, filename=name)
+
